074e0dcba6bd41d1b916a7e6346deeaa280ed980
Andreas Ek <andreas@andreasek.se>
2016-04-17 11:43:54 +0200

    Logo on dark in header

--- resources/views/parts/logo.blade.php
+++ resources/views/parts/logo.blade.php
@@ -1,7 +1,7 @@
     <!-- Logo -->
     <a href="/" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><img style="height:30px;margin-top:-5px" src="{{ asset('assets/images/qbus.png') }}"/> <b>Q</b>bus</span>
+      <span class="logo-mini"><img style="height:30px;margin-top:-5px" src="{{ asset('assets/images/qbus-on-dark.png') }}"/> <b>Q</b>bus</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><img style="height:30px;margin-top:-5px" src="{{ asset('assets/images/qbus.png') }}"/> <b>Q</b>bus</span>
+      <span class="logo-lg"><img style="height:30px;margin-top:-5px" src="{{ asset('assets/images/qbus-on-dark.png') }}"/> <b>Q</b>bus</span>
     </a>
