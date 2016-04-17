a255d71a376e19a8018a87c5b9f6359db981a79e
Andreas Ek <andreas@andreasek.se>
2016-04-17 13:40:12 +0200

    Qbus footer

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.1
     </div>
-    <strong>Levereras med öppen källkod från <a href="http://www.aekab.se">AEKAB</a>.</strong>
+    <strong>Levereras med öppen källkod från <a href="http://www.aekab.se">QBUS</a>.</strong>
   </footer>
